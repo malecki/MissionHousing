9b9c52e8c34ef3c9ca20d5be93fef1cbe9603f90
Vivian Peng <vpeng12@gmail.com>
2014-03-26 22:13:51 -0400

    pulled demographic data

---
+++ data/census/R/RaceEthnicity.R
@@ -0,0 +1,85 @@
+setwd("~/git/MissionHousing/data/census/R")
+
+require(stringr)
+require(plyr)
+require(reshape)
+
+# get list of the files
+
+
+theFiles <- dir(path="../2009-2012-RaceEthnicity/", pattern = "\\.csv")
+
+for(a in theFiles)
+{
+  # build a good name to assign to the data
+  nameToUse <- str_sub(string=a, start=5, end=26)
+  # year <- str_sub(string=a, start=5, end=6)
+  
+  # read in the csv using read.table
+  # file.path is a convenient way to specify a folder and file name
+  temp <- read.table(file = file.path("../2009-2012-RaceEthnicity/", a), 
+                     header = FALSE, 
+                     sep = ",", 
+                     stringsAsFactors=FALSE)
+  
+  # assign them into the workspace
+  assign(x = nameToUse, value = temp)
+  
+  # assign metadata dataframe to dataframe
+  search <- str_sub(string=nameToUse, start = 15, end = 22)
+  if ( search == "metadata")
+  {
+    df.metadata <-  temp
+  }
+  
+  # assign annotations dataframe to dataframe
+  # transpose annotations df
+  # merge metadata and transposed annotations
+  if (search == "with_ann")
+  {
+    df.annotations <- temp
+    df.annotations.transposed <- as.data.frame(t(df.annotations))
+    names(df.annotations.transposed) <- c("key", "SFCounty","T208", "T209", "T228.01", "T228.02", "T228.03", "T229.01", 
+                                          "T229.02", "T229.03")
+    nameOfMerged <- str_sub(string=a, start=5, end=12)
+    df.merged <- merge(x= df.metadata, y = df.annotations.transposed, by.x="V1", by.y="key", stringsAsFactors=FALSE)
+    
+    # assign them into the workspace
+    assign(x = nameOfMerged, value = df.merged)
+  }
+}
+
+
+# only looking for estimates
+# create table per year
+Race2009 <- `09_5YR_B`[4:24,2:11]
+Race2010 <- `10_5YR_B`[4:24,2:11]
+Race2011 <- `11_5YR_B`[4:24,2:11]
+Race2012 <- `12_5YR_B`[4:24,2:11]
+class(Race2012$T208)
+
+# make a year column values numerics
+y <- c(2009, 2010, 2011, 2012)
+dfs <- list (`09_5YR_B`, `10_5YR_B`,`11_5YR_B`, `12_5YR_B`)
+# sum estimates across all census tracks
+Housing2010$Total2010 <- rowSums(Housing2010 [,3:10])
+Housing2011$Total2011 <- rowSums(Housing2011 [,3:10])
+Housing2012$Total2012 <- rowSums(Housing2012 [,3:10])
+
+# Combine the data tables together
+RaceData <- cbind(`09_5YR_B`[,c(2,11)], `10_5YR_B`[,c(2,11)],`11_5YR_B`[,c(2,11)], `12_5YR_B`[,c(2,11)])
+RaceData <- RaceData[,c(1:2,4,6,8)]
+# # add a year column to the data frame
+# y <- seq(from=2010, to= 2012)
+# Years <- rep(y, times = 1, length.out = 387, each = 129)
+# HousingData$Years <- Years
+
+# Remove 'Estimate;' from row names
+HousingData[,1] <- gsub("Estimate; ", "", HousingData[,1])
+
+# write out data to csv file
+write.table(HousingData, "HousingData2010-2012.csv", sep=",", col.names= TRUE, row.names = FALSE)
+
+
+
+
